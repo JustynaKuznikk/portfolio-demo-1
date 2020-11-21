2d9091a17fa61c753cd86c9cd26ab4b207011028
honestit <michal.kupisinski@gmail.com>
2020-11-21 14:42:39 +0100

    register-user :: front validation and object binding

--- src/main/java/projects/portfoliodemo/web/controller/RegistrationController.java
+++ src/main/java/projects/portfoliodemo/web/controller/RegistrationController.java
@@ -1,20 +1,28 @@
 package projects.portfoliodemo.web.controller;
 
+import com.zaxxer.hikari.metrics.MetricsTrackerFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import projects.portfoliodemo.service.UserService;
 import projects.portfoliodemo.web.command.RegisterUserCommand;
+
+import javax.validation.Valid;
 
 @Controller @Slf4j @RequiredArgsConstructor
 @RequestMapping("/register")
 public class RegistrationController {
 
+    private final UserService userService;
+
     @GetMapping
-    public String getRegister() {
+    public String getRegister(Model model) {
+        model.addAttribute(new RegisterUserCommand());
         return "register/form";
     }
 
@@ -25,10 +33,15 @@ public class RegistrationController {
     // - RegisterUserForm
     // - RegisterUserRequest
     // - RegisterUserCommand
-    public String processRegister(RegisterUserCommand registerUserCommand, BindingResult bindingResult) {
+    public String processRegister(@Valid RegisterUserCommand registerUserCommand, BindingResult bindingResult) {
+        log.debug("Dane do utworzenia użytkownika: {}", registerUserCommand);
         if (bindingResult.hasErrors()) {
+            log.debug("Błędne dane: {}", bindingResult.getAllErrors());
             return "register/form";
         }
+
+        Long id = userService.create(registerUserCommand);
+        log.debug("Utworzono użytkownika o id = {}", id);
         return "redirect:/login";
     }
 }
