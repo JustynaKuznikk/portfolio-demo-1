1272f29518c5600cca9cbe2513079fdb01df69e5
honestit <michal.kupisinski@gmail.com>
2020-11-22 13:01:13 +0100

    add-project :: uzupełniona klasa encji

--- src/main/java/projects/portfoliodemo/domain/model/Project.java
+++ src/main/java/projects/portfoliodemo/domain/model/Project.java
@@ -1,5 +1,9 @@
 package projects.portfoliodemo.domain.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+
 /**
  * Encja reprezentująca projekt z informacjami o:
  * - nazwa
@@ -7,5 +11,30 @@ package projects.portfoliodemo.domain.model;
  * - opis
  * - relacja do użytkownika wiele projektów do jednego użytkownika
  */
+@Entity
+@Table(
+        name = "projects",
+        uniqueConstraints = @UniqueConstraint(
+                name = "projects_name_user_id_idx",
+                columnNames = {"name", "user_id"}))
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "id")
+@ToString(exclude = "user")
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    private String url;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
