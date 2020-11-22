570f64c0903a554b9f64b742a04f45b775e9a791
honestit <michal.kupisinski@gmail.com>
2020-11-22 14:01:46 +0100

    add-project :: kontroler i obsługa ządania GET

--- src/main/java/projects/portfoliodemo/web/controller/AddNewProjectController.java
+++ src/main/java/projects/portfoliodemo/web/controller/AddNewProjectController.java
@@ -1,4 +1,22 @@
 package projects.portfoliodemo.web.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import projects.portfoliodemo.web.command.CreateProjectCommand;
+
+@Controller
+@RequestMapping("/projects")
+@Slf4j
+@RequiredArgsConstructor
 public class AddNewProjectController {
+
+    @GetMapping
+    public String getAddProjectPage(Model model) {
+        model.addAttribute(new CreateProjectCommand());
+        return "projects/add";
+    }
 }
