94ad3a98c45dff20a666b3baef4c002c9754b597
honestit <michal.kupisinski@gmail.com>
2020-11-22 14:45:14 +0100

    add-project :: obsługa tworzenia projektu w metodzie POST kontrolera

--- src/main/java/projects/portfoliodemo/service/ProjectService.java
+++ src/main/java/projects/portfoliodemo/service/ProjectService.java
@@ -1,4 +1,16 @@
 package projects.portfoliodemo.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import projects.portfoliodemo.web.command.CreateProjectCommand;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class ProjectService {
+
+    public void add(CreateProjectCommand createProjectCommand) {
+
+    }
 }
--- src/main/java/projects/portfoliodemo/web/controller/AddNewProjectController.java
+++ src/main/java/projects/portfoliodemo/web/controller/AddNewProjectController.java
@@ -4,9 +4,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import projects.portfoliodemo.service.ProjectService;
 import projects.portfoliodemo.web.command.CreateProjectCommand;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/projects")
@@ -14,9 +19,31 @@ import projects.portfoliodemo.web.command.CreateProjectCommand;
 @RequiredArgsConstructor
 public class AddNewProjectController {
 
+    private final ProjectService projectService;
+
     @GetMapping("/add")
     public String getAddProjectPage(Model model) {
         model.addAttribute(new CreateProjectCommand());
         return "project/add";
+    }
+
+    @PostMapping
+    public String processAddProject(@Valid CreateProjectCommand createProjectCommand, BindingResult bindings) {
+        log.debug("Dane do utworzenia projektu: {}", createProjectCommand);
+        if (bindings.hasErrors()) {
+            log.debug("Dane zawierają błędy: {}", bindings.getAllErrors());
+            return "project/add";
+        }
+
+        try {
+            projectService.add(createProjectCommand);
+            log.debug("Utworzono projekt");
+            return "redirect:/projects";
+        } catch (RuntimeException re) {
+            log.warn(re.getLocalizedMessage());
+            log.debug("Błąd podczas tworzenia projektu", re);
+            bindings.rejectValue(null, null, "Wystąpił błąd");
+            return "project/add";
+        }
     }
 }
