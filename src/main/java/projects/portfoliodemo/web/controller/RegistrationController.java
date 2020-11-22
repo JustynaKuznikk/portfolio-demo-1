a2eee2f07fb08fec8e1c1099d455181b88925b2e
honestit <michal.kupisinski@gmail.com>
2020-11-22 09:47:36 +0100

    user-profile :: model classes

--- src/main/java/projects/portfoliodemo/web/controller/RegistrationController.java
+++ src/main/java/projects/portfoliodemo/web/controller/RegistrationController.java
@@ -1,12 +1,10 @@
 package projects.portfoliodemo.web.controller;
 
-import com.zaxxer.hikari.metrics.MetricsTrackerFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
