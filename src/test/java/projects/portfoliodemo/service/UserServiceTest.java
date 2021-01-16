019db6b1f1ccb4a853cac185547402484e4542f0
honestit <michal.kupisinski@gmail.com>
2021-01-16 12:40:09 +0100

    unit-tests-with-mocks: user service
    przygotowanie klasy testowej

---
+++ src/test/java/projects/portfoliodemo/service/UserServiceTest.java
@@ -0,0 +1,32 @@
+package projects.portfoliodemo.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import projects.portfoliodemo.converter.UserConverter;
+import projects.portfoliodemo.domain.repository.UserRepository;
+
+@DisplayName("User processes specification")
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    UserConverter userConverter;
+    @Mock
+    UserRepository userRepository;
+    @Mock
+    PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    UserService cut;
+
+    @BeforeEach
+    void setUp() {
+        //
+    }
+
+}
