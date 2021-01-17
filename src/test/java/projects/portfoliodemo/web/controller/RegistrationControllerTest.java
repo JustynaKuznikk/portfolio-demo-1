0f0a73518b569ac7646df055898d4b315fcb47ba
honestit <michal.kupisinski@gmail.com>
2021-01-17 14:13:58 +0100

    web-tests: registration controller
    rejestracja nowego użytkownika

--- src/test/java/projects/portfoliodemo/web/controller/RegistrationControllerTest.java
+++ src/test/java/projects/portfoliodemo/web/controller/RegistrationControllerTest.java
@@ -3,14 +3,17 @@ package projects.portfoliodemo.web.controller;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import projects.portfoliodemo.provider.CommandProvider;
 import projects.portfoliodemo.security.CustomUserDetailsService;
 import projects.portfoliodemo.service.UserService;
 import projects.portfoliodemo.web.command.RegisterUserCommand;
@@ -53,6 +56,29 @@ class RegistrationControllerTest {
             mockMvc.perform(MockMvcRequestBuilders.get(endpoint)
                     .with(SecurityMockMvcRequestPostProcessors.anonymous()))
                     .andExpect(MockMvcResultMatchers.status().isOk());
+        }
+    }
+
+    @DisplayName("2. On POST request")
+    @Nested
+    class PostRequest {
+
+        @DisplayName("- should create user when data is correct")
+        @Test
+        void test1() throws Exception {
+            RegisterUserCommand command = CommandProvider.registerUserCommand("duke", "pass");
+            Mockito.when(userService.create(command)).thenReturn(22L);
+
+            mockMvc.perform(MockMvcRequestBuilders.post(endpoint)
+                    .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                    .param("username", "duke")
+                    .param("password", "pass")
+                    .with(SecurityMockMvcRequestPostProcessors.anonymous())
+                    .with(SecurityMockMvcRequestPostProcessors.csrf()))
+                    .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                    .andExpect(MockMvcResultMatchers.redirectedUrl("/login"));
+
+            Mockito.verify(userService, Mockito.times(1)).create(command);
         }
 
     }
