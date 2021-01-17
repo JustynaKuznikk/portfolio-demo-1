ddd88c9885d46afdb86930f290dcb64f80bbcaa5
honestit <michal.kupisinski@gmail.com>
2021-01-17 13:27:32 +0100

    web-tests: registration controller
    test uzyskania widoku

--- src/test/java/projects/portfoliodemo/web/controller/RegistrationControllerTest.java
+++ src/test/java/projects/portfoliodemo/web/controller/RegistrationControllerTest.java
@@ -1,12 +1,18 @@
 package projects.portfoliodemo.web.controller;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import projects.portfoliodemo.security.CustomUserDetailsService;
 import projects.portfoliodemo.service.UserService;
 
-@DisplayName("Web Registration Specification")
+@DisplayName("Web Registration Specification: /register")
 @WebMvcTest(RegistrationController.class)
 class RegistrationControllerTest {
 
@@ -15,5 +21,23 @@ class RegistrationControllerTest {
 
     @MockBean
     UserService userService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    String endpoint = "/register";
+
+    // MockMvcRequestBuilders -> do tworzenia żądań
+    // MockMvc
+
+    @DisplayName("- should get registration view on GET request")
+    @Test
+    void test1() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(endpoint))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("registration/form"));
+    }
+
 
 }
