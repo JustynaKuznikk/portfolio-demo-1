b5f12f891773f7fa8e4f68dec81a3284b5b4aab6
honestit <michal.kupisinski@gmail.com>
2021-01-17 13:31:38 +0100

    web-tests: registration controller
    test uzyskania widoku: implementacja

--- src/main/java/projects/portfoliodemo/web/controller/RegistrationController.java
+++ src/main/java/projects/portfoliodemo/web/controller/RegistrationController.java
@@ -3,12 +3,20 @@ package projects.portfoliodemo.web.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import projects.portfoliodemo.service.UserService;
 
-@Controller @RequestMapping("/register")
-@Slf4j @RequiredArgsConstructor
+@Controller
+@RequestMapping("/register")
+@Slf4j
+@RequiredArgsConstructor
 public class RegistrationController {
 
     private final UserService userService;
+
+    @GetMapping
+    public String preparePage() {
+        return "registration/form";
+    }
 }
