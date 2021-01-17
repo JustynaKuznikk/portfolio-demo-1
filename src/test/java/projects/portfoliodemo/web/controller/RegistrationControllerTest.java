9a9246d7e8afad1d831505b63e645c53913ee2ae
honestit <michal.kupisinski@gmail.com>
2021-01-17 13:45:07 +0100

    web-tests: registration controller
    refaktoryzacja + test security

--- src/test/java/projects/portfoliodemo/web/controller/RegistrationControllerTest.java
+++ src/test/java/projects/portfoliodemo/web/controller/RegistrationControllerTest.java
@@ -1,10 +1,12 @@
 package projects.portfoliodemo.web.controller;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -13,7 +15,7 @@ import projects.portfoliodemo.security.CustomUserDetailsService;
 import projects.portfoliodemo.service.UserService;
 import projects.portfoliodemo.web.command.RegisterUserCommand;
 
-@DisplayName("Web Registration Specification: /register")
+@DisplayName("Registration Specification: /register")
 @WebMvcTest(RegistrationController.class)
 class RegistrationControllerTest {
 
@@ -31,20 +33,28 @@ class RegistrationControllerTest {
     // MockMvcRequestBuilders -> do tworzenia żądań
     // MockMvc
 
-    @DisplayName("- should get registration view on GET request")
-    @Test
-    void test1() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get(endpoint))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("registration/form"));
-    }
+    @DisplayName("1. On GET request")
+    @Nested
+    class GetRequest {
 
-    @DisplayName("- should prepare model for registration view on GET request")
-    @Test
-    void test2() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get(endpoint))
-                .andExpect(MockMvcResultMatchers.model().attribute("data", new RegisterUserCommand()));
+        @DisplayName("- should prepare registration view with data in model")
+        @Test
+        void test1() throws Exception {
+            mockMvc.perform(MockMvcRequestBuilders.get(endpoint))
+                    .andDo(MockMvcResultHandlers.print())
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.view().name("registration/form"))
+                    .andExpect(MockMvcResultMatchers.model().attribute("data", new RegisterUserCommand()));
+        }
+
+        @DisplayName("- should allow anonymous user")
+        @Test
+        void test2() throws Exception {
+            mockMvc.perform(MockMvcRequestBuilders.get(endpoint)
+                    .with(SecurityMockMvcRequestPostProcessors.anonymous()))
+                    .andExpect(MockMvcResultMatchers.status().isOk());
+        }
+
     }
 
 
