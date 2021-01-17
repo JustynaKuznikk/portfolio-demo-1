2571680de5b0350944c799912cdd46a973283435
honestit <michal.kupisinski@gmail.com>
2021-01-17 09:57:16 +0100

    unit-tests-with-mocks: user service
    refaktoryzacja testów do pojedynczego testu podstawowego przebiegu

--- src/test/java/projects/portfoliodemo/service/UserServiceTest.java
+++ src/test/java/projects/portfoliodemo/service/UserServiceTest.java
@@ -15,10 +15,7 @@ import projects.portfoliodemo.domain.repository.UserRepository;
 import projects.portfoliodemo.provider.CommandProvider;
 import projects.portfoliodemo.web.command.RegisterUserCommand;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("User processes specification")
 @ExtendWith(MockitoExtension.class)
@@ -45,68 +42,35 @@ class UserServiceTest {
     @Nested
     class CreateUser {
 
-        @DisplayName("- should save user and return they id")
+        @DisplayName("- should save user with provided data and set default values, and return user id")
         @Test
         void test1() {
-            Mockito.when(userConverter.from(any(RegisterUserCommand.class))).thenReturn(new User());
-            Mockito.when(userRepository.save(ArgumentMatchers.any(User.class)))
+            RegisterUserCommand command = CommandProvider.registerUserCommand("duke", "pass");
+            User convertedUser = User.builder()
+                    .username("duke")
+                    .password("pass")
+                    .build();
+
+            Mockito.when(userConverter.from(command)).thenReturn(convertedUser);
+            Mockito.when(passwordEncoder.encode("pass")).thenReturn("encoded");
+            Mockito.when(userRepository.save(userCaptor.capture()))
                     .thenAnswer(invocation -> {
                         User userToSave = invocation.getArgument(0, User.class);
                         userToSave.setId(99L);
                         return userToSave;
                     });
 
-            RegisterUserCommand command = CommandProvider.registerUserCommand("duke", "pass");
-
             Long result = cut.create(command);
 
-            verify(userRepository, times(1)).save(any(User.class));
-
-            assertEquals(99L, result);
-        }
-
-        @DisplayName("- should save user with provided data")
-        @Test
-        void test2() {
-            RegisterUserCommand command = CommandProvider.registerUserCommand("duke", "pass");
-            User expectedToSave = User.builder()
-                    .username("duke")
-                    .password("pass")
-                    .build();
-
-            Mockito.when(userConverter.from(command)).thenReturn(expectedToSave);
-
-            cut.create(command);
-
-            Mockito.verify(userRepository, Mockito.atLeastOnce()).save(userCaptor.capture());
             User savedUser = userCaptor.getValue();
-
-            Assertions.assertThat(savedUser)
-                    .isEqualToComparingOnlyGivenFields(expectedToSave, "username", "password");
-        }
-
-        @DisplayName("- should save user as active with user role and encoded password")
-        @Test
-        void test3() {
-            RegisterUserCommand command = CommandProvider.registerUserCommand("duke", "pass");
-            User expectedToSave = User.builder()
-                    .username("duke")
-                    .password("pass")
-                    .build();
-
-            Mockito.when(passwordEncoder.encode("pass")).thenReturn("encoded");
-            Mockito.when(userConverter.from(command)).thenReturn(expectedToSave);
-
-            cut.create(command);
-
-            Mockito.verify(userRepository, atLeastOnce()).save(userCaptor.capture());
-            User savedUser = userCaptor.getValue();
-
+            assertNotNull(savedUser);
+            assertEquals("duke", savedUser.getUsername());
             assertTrue(savedUser.getActive());
             Assertions.assertThat(savedUser.getRoles()).containsOnly("ROLE_USER");
             assertEquals("encoded", savedUser.getPassword());
-        }
 
+            assertEquals(99L, result);
+        }
     }
 
 
